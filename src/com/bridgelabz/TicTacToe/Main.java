f694b49d20fe2c389f8e1ddc0e76eb6302bb0aeb
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-27 08:52:12 +0530

    [Hitesh]: Creating TicTacToe Board

---
+++ src/com/bridgelabz/TicTacToe/Main.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.TicTacToe;
+/**
+ * 
+ * @author HITESH
+ *
+ */
+public class Main {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * calling two methods
+		 */
+		TicTacToeGame.TicTacToeGame();
+		TicTacToeGame.printBoard();
+	}
+}
